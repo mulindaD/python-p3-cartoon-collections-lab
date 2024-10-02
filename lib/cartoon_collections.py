a80627eb05c2a380419d9905c8a13e328bd19465
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-10-02 21:34:54 +0300

    Complete cartoon collections

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,34 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    num = 0
+    for name in names:
+        num += 1
+        print (f"{num}. {name}")
+        # return f"{num}. {name}"
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planets):
+    # planet_list = []
 
-def long_planeteer_calls():
-    pass
+    # for planet in planets:
+    #     planet_list.append(planet.title() + "!")
+    # return planet_list
+    return [planet.title() + "!" for planet in planets]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(words):
+    truthy =[]
+    for word in words:
+        if len(word) > 4:
+            truthy.append(True)
+        else:
+            truthy.append(False)
+    return True in truthy 
+
+cheese = ["cheddar", "gouda", "camembert"]
+
+def find_the_cheese(food_list):
+    cheese_list = []
+    for food in food_list:
+        if food in cheese:
+            cheese_list.append(food)
+            
+    return cheese_list[0] if cheese_list else None
+
